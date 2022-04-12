c6659c14be43168b4448a67a282d1f9f720c95a2
grewaljai <98025330+grewaljai@users.noreply.github.com>
2022-04-12 15:57:13 +0530

    Create test.php

---
+++ test.php
@@ -0,0 +1,12 @@
+# rice.github.io
+<!doctype html>
+<html>
+  <head>
+    <title>PHP Textbook</title>
+  </head>
+  <body>
+    <p>
+      <?php echo "Hello World!"; ?>
+    </p>
+  </body>
+</html>
